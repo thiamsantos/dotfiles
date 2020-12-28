5514eff4bf8003d2edb98380d8d64b68ffcf8278
Thiago Santos <thia.mdossantos@gmail.com>
2020-12-28 16:02:53 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
+set -e
+
 export HOME="/home/thiago"
 DOTFILES="$HOME/dotfiles"
 
-git config --global user.name "Thiago Santos" && \
-	git config --global user.email thia.mdossantos@gmail.com
-
 sudo apt update && \
-	sudo apt install --yes git ansible
+  sudo apt install --yes git ansible
+
+git config --global user.name "Thiago Santos" && \
+  git config --global user.email thia.mdossantos@gmail.com
 
 if [ ! -d $DOTFILES ]; then
-    git clone git@github.com:thiamsantos/dotfiles.git $DOTFILES
+  git clone git@github.com:thiamsantos/dotfiles.git $DOTFILES
 fi
 
 cd $DOTFILES && \
-	git pull --rebase && \
-	ansible-playbook -K -i hosts "$DOTFILES/setup.yml"
+  git pull --rebase && \
+  ansible-playbook -K -i hosts "$DOTFILES/setup.yml"
