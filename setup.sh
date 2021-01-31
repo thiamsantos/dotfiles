921c53782167d1faf729c6a653ccc46f70689b8d
Thiago Santos <thia.mdossantos@gmail.com>
2021-01-31 16:49:19 -0300

    Install awscli

--- setup.sh
+++ setup.sh
@@ -5,8 +5,9 @@ set -e
 export HOME="/home/thiago"
 DOTFILES="$HOME/dotfiles"
 
-sudo apt update && \
-  sudo apt install --yes git ansible
+sudo apt update &&
+  sudo apt install --yes git python3 python3-venv python3-pip && \
+  pip3 install ansible
 
 git config --global user.name "Thiago Santos" && \
   git config --global user.email thia.mdossantos@gmail.com
@@ -17,5 +18,5 @@ fi
 
 cd $DOTFILES && \
   git pull --rebase && \
-  ansible-galaxy collection install -r requirements.yml && \
-  ANSIBLE_NOCOWS=1 ansible-playbook -K -i hosts "$DOTFILES/setup.yml"
+  ANSIBLE_PYTHON_INTERPRETER=/usr/bin/python3 ANSIBLE_NOCOWS=1 ansible-galaxy collection install -r requirements.yml && \
+  ANSIBLE_PYTHON_INTERPRETER=/usr/bin/python3 ANSIBLE_NOCOWS=1 ansible-playbook -K -i hosts "$DOTFILES/setup.yml"
