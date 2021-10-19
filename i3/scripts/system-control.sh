46d7781bb44d3b03e903b40f1a112f8aaab34a0b
Thiago Santos <thia.mdossantos@gmail.com>
2021-10-19 13:05:14 -0300

    wip

---
+++ i3/scripts/system-control.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+
+chosen=$(echo "[Cancel]\nLock\nLogout\nSuspend\nHibernate\nReboot\nShutdown" | rofi -dmenu -i)
+
+case $chosen in
+    Lock)
+        sh ~/.config/i3/scripts/lock.sh
+        ;;
+    Logout)
+        i3-msg exit
+        ;;
+    Suspend)
+        sh ~/.config/i3/scripts/lock.sh && systemctl suspend
+        ;;
+    Hibernate)
+        sh ~/.config/i3/script/lock.sh && systemctl suspend
+        ;;
+    Reboot)
+        systemctl reboot
+        ;;
+    Shutdown)
+        systemctl systemctl poweroff -i
+        ;;
+    *)
+        exit 0
+        ;;
+esac
