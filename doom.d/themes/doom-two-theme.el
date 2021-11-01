8cb50a0901d22156b05bfa3e16a85c18c6092f5d
Thiago Santos <thia.mdossantos@gmail.com>
2021-11-01 18:40:28 -0300

    wip

--- doom.d/themes/doom-two-theme.el
+++ doom.d/themes/doom-two-theme.el
@@ -1,10 +1,39 @@
-;;; doom-two-theme.el --- inspired by dark+ Theme by equinusocio -*- lexical-binding: t; no-byte-compile: t; -*-
+;;; doom-two-theme.el --- inspired by Atom One Dark -*- lexical-binding: t; no-byte-compile: t; -*-
+;;
+;; Copyright (C) 2016-2021 Henrik Lissner
+;;
+;; Author: Henrik Lissner <https://github.com/hlissner>
+;; Created: December 6, 2020
+;; Version: 2.0.0
+;; Keywords: custom themes, faces
+;; Homepage: https://github.com/hlissner/emacs-doom-themes
+;; Package-Requires: ((emacs "25.1") (cl-lib "0.5") (doom-themes "2.2.1"))
+;;
+;;; Commentary:
+;;
+;; Inspired by Atom's One Dark color scheme.
+;;
+;;; Code:
+
 (require 'doom-themes)
 
+
 ;;
+;;; Variables
+
 (defgroup doom-two-theme nil
   "Options for the `doom-two' theme."
   :group 'doom-themes)
+
+(defcustom doom-two-brighter-modeline nil
+  "If non-nil, more vivid colors will be used to style the mode-line."
+  :group 'doom-two-theme
+  :type 'boolean)
+
+(defcustom doom-two-brighter-comments nil
+  "If non-nil, comments will be highlighted in more vivid colors."
+  :group 'doom-two-theme
+  :type 'boolean)
 
 (defcustom doom-two-padded-modeline doom-themes-padded-modeline
   "If non-nil, adds a 4px padding to the mode-line.
@@ -12,175 +41,140 @@ Can be an integer to determine the exact padding."
   :group 'doom-two-theme
   :type '(choice integer boolean))
 
-(defcustom doom-two-blue-modeline nil
-  "If non-nil, mode-line's color will be blue instead of the default purple."
-  :group 'doom-two-theme
-  :type '(choice integer boolean))
 
 ;;
+;;; Theme definition
+
 (def-doom-theme doom-two
-  "A dark theme inspired by dark+ Theme by equinusocio"
+  "A dark theme inspired by Atom One Dark."
 
-  ;; name        default   256       16
-  ((bg         '("#1e1e1e" "#1e1e1e" nil))
-   (bg-alt     '("#252526" "#222222"  nil))
-   (base0      '("#171F24" "#111122"   "black"))
-   (base1      '("#1C1C1C" "#1C1C1C" "brightblack"))
-   (base2      '("#121212" "#626262" "brightblack"))
-   (base3      '("#313131" "#3a3a3a" "brightblack"))
-   (base4      '("#4b474c" "#444444" "brightblack"))
-   (base5      '("#37474F" "#585858" "brightblack"))
-   (base6      '("#237AD3" "#2277DD" "brightblack"))
-   (base7      '("#777778" "#767676" "brightblack"))
-   (base8      '("#f4f4f4" "#a8a8a8" "white"))
-   (fg         '("#d4d4d4" "#e4e4e4" "brightwhite"))
-   (fg-alt     '("#AEAFAD" "#bcbcbc" "white"))
+  ;; name        default   256           16
+  ((bg         '("#282c34" "black"       "black"  ))
+   (fg         '("#bbc2cf" "#bfbfbf"     "brightwhite"  ))
 
-   (grey base7)
-   (red          '("#D16969" "#DD6666" "red"))
-   (orange       '("#DB8E73" "#DD8877" "brightred"))
-   (green        '("#579C4C" "#559944" "green"))
-   (light-green  '("#B5CEA8" "#BBCCAA" "green"))
-   (teal         '("#35CDAF" "#33CCAA" "brightgreen"))
-   (yellow       '("#D7BA7D" "#DDBB77" "brightyellow"))
-   (light-yellow '("#D9DAA2" "#DDDDAA" "brightyellow"))
-   (blue         '("#339CDB" "#3399DD" "brightblue"))
-   (dark-blue    '("#124F7B" "#114477" "blue"))
-   (magenta      '("#C586C0" "#CC88CC" "brightmagenta"))
-   (violet       '("#BB80B3" "#BB88BB" "magenta"))
-   (dark-violet  '("#68217A" "#662277" "magenta"))
-   (cyan         '("#85DDFF" "#5FD7FF" "brightcyan"))
-   (dark-cyan    '("#207FA1" "#2277AA" "cyan"))
+   ;; These are off-color variants of bg/fg, used primarily for `solaire-mode',
+   ;; but can also be useful as a basis for subtle highlights (e.g. for hl-line
+   ;; or region), especially when paired with the `doom-darken', `doom-lighten',
+   ;; and `doom-blend' helper functions.
+   (bg-alt     '("#21242b" "black"       "black"        ))
+   (fg-alt     '("#5B6268" "#2d2d2d"     "white"        ))
 
-   ;; face categories -- required for all themes
-   (highlight      base6)
-   (vertical-bar   bg-alt)
-   (selection      base4)
+   ;; These should represent a spectrum from bg to fg, where base0 is a starker
+   ;; bg and base8 is a starker fg. For example, if bg is light grey and fg is
+   ;; dark grey, base0 should be white and base8 should be black.
+   (base0      '("#1B2229" "black"       "black"        ))
+   (base1      '("#1c1f24" "#1e1e1e"     "brightblack"  ))
+   (base2      '("#202328" "#2e2e2e"     "brightblack"  ))
+   (base3      '("#23272e" "#262626"     "brightblack"  ))
+   (base4      '("#3f444a" "#3f3f3f"     "brightblack"  ))
+   (base5      '("#5B6268" "#525252"     "brightblack"  ))
+   (base6      '("#73797e" "#6b6b6b"     "brightblack"  ))
+   (base7      '("#9ca0a4" "#979797"     "brightblack"  ))
+   (base8      '("#DFDFDF" "#dfdfdf"     "white"        ))
+
+   (grey       base4)
+   (red        '("#ff6c6b" "#ff6655" "red"          ))
+   (orange     '("#da8548" "#dd8844" "brightred"    ))
+   (green      '("#98be65" "#99bb66" "green"        ))
+   (teal       '("#4db5bd" "#44b9b1" "brightgreen"  ))
+   (yellow     '("#ECBE7B" "#ECBE7B" "yellow"       ))
+   (blue       '("#51afef" "#51afef" "brightblue"   ))
+   (dark-blue  '("#2257A0" "#2257A0" "blue"         ))
+   (magenta    '("#c678dd" "#c678dd" "brightmagenta"))
+   (violet     '("#a9a1e1" "#a9a1e1" "magenta"      ))
+   (cyan       '("#46D9FF" "#46D9FF" "brightcyan"   ))
+   (dark-cyan  '("#5699AF" "#5699AF" "cyan"         ))
+
+   ;; These are the "universal syntax classes" that doom-themes establishes.
+   ;; These *must* be included in every doom themes, or your theme will throw an
+   ;; error, as they are used in the base theme defined in doom-themes-base.
+   (highlight      blue)
+   (vertical-bar   (doom-darken base1 0.1))
+   (selection      dark-blue)
    (builtin        magenta)
-   (comments       green)
-   (doc-comments   base7)
-   (constants      blue)
-   (functions      light-yellow)
+   (comments       (if doom-two-brighter-comments dark-cyan base5))
+   (doc-comments   (doom-lighten (if doom-two-brighter-comments dark-cyan base5) 0.25))
+   (constants      violet)
+   (functions      magenta)
    (keywords       blue)
-   (methods        light-yellow)
-   (operators      cyan)
-   (type           teal)
-   (strings        orange)
-   (variables      cyan)
-   (numbers        light-green)
-   (region         (doom-darken base6 0.5))
+   (methods        cyan)
+   (operators      blue)
+   (type           yellow)
+   (strings        green)
+   (variables      (doom-lighten magenta 0.4))
+   (numbers        orange)
+   (region         `(,(doom-lighten (car bg-alt) 0.15) ,@(doom-lighten (cdr base1) 0.35)))
    (error          red)
    (warning        yellow)
    (success        green)
-   (vc-modified    blue)
+   (vc-modified    orange)
    (vc-added       green)
    (vc-deleted     red)
 
-   ;; custom categories
-   (modeline-bg     (if doom-two-blue-modeline base6 base3))
-   (modeline-bg-alt (doom-darken bg 0.01))
-   (modeline-fg     base8)
-   (modeline-fg-alt blue)
+   ;; These are extra color variables used only in this theme; i.e. they aren't
+   ;; mandatory for derived themes.
+   (modeline-fg              fg)
+   (modeline-fg-alt          base5)
+   (modeline-bg              (if doom-two-brighter-modeline
+                                 (doom-darken blue 0.45)
+                               (doom-darken bg-alt 0.1)))
+   (modeline-bg-alt          (if doom-two-brighter-modeline
+                                 (doom-darken blue 0.475)
+                               `(,(doom-darken (car bg-alt) 0.15) ,@(cdr bg))))
+   (modeline-bg-inactive     `(,(car bg-alt) ,@(cdr base1)))
+   (modeline-bg-inactive-alt `(,(doom-darken (car bg-alt) 0.1) ,@(cdr bg)))
 
    (-modeline-pad
     (when doom-two-padded-modeline
       (if (integerp doom-two-padded-modeline) doom-two-padded-modeline 4))))
 
+
   ;;;; Base theme face overrides
-  ((lazy-highlight :background base4 :foreground fg :distant-foreground fg :weight 'bold)
+  (((line-number &override) :foreground base4)
+   ((line-number-current-line &override) :foreground fg)
+   ((font-lock-comment-face &override)
+    :background (if doom-two-brighter-comments (doom-lighten bg 0.05)))
    (mode-line
     :background modeline-bg :foreground modeline-fg
     :box (if -modeline-pad `(:line-width ,-modeline-pad :color ,modeline-bg)))
    (mode-line-inactive
-    :background modeline-bg-alt :foreground modeline-fg-alt
-    :box (if -modeline-pad `(:line-width ,-modeline-pad :color ,modeline-bg-alt)))
-   (mode-line-emphasis
-    :foreground fg
-    :weight 'bold)
+    :background modeline-bg-inactive :foreground modeline-fg-alt
+    :box (if -modeline-pad `(:line-width ,-modeline-pad :color ,modeline-bg-inactive)))
+   (mode-line-emphasis :foreground (if doom-two-brighter-modeline base8 highlight))
 
-   ;;;; all-the-icons
-   (all-the-icons-dblue :foreground blue)
-   ;;;; centaur-tabs
-   (centaur-tabs-active-bar-face :background base6)
-   (centaur-tabs-selected-modified
-    :inherit 'centaur-tabs-selected :foreground fg :weight 'bold)
-   (centaur-tabs-unselected-modified
-    :inherit 'centaur-tabs-unselected :foreground fg :weight 'bold)
-   (centaur-tabs-modified-marker-selected
-    :inherit 'centaur-tabs-selected :foreground fg)
-   (centaur-tabs-modified-marker-unselected
-    :inherit 'centaur-tabs-unselected :foreground fg)
-   ;;;; company
-   (company-tooltip-selection     :background region)
    ;;;; css-mode <built-in> / scss-mode
    (css-proprietary-property :foreground orange)
    (css-property             :foreground green)
    (css-selector             :foreground blue)
-   ;;;; dashboard
-   (dashboard-heading :foreground green :weight 'bold)
-   ;;;; dired-k
-   (dired-k-commited :foreground base4)
-   (dired-k-modified :foreground vc-modified)
-   (dired-k-ignored :foreground cyan)
-   (dired-k-added    :foreground vc-added)
    ;;;; doom-modeline
-   (doom-modeline-bar :background (if doom-two-blue-modeline base6 base3))
-   (doom-modeline-buffer-file :inherit 'mode-line-emphasis :weight 'bold)
-   (doom-modeline-buffer-major-mode :inherit 'doom-modeline-buffer-path)
-   (doom-modeline-buffer-modified :inherit 'mode-line-emphasis :weight 'bold)
-   (doom-modeline-debug :inherit 'mode-line-emphasis)
-   (doom-modeline-evil-insert-state :foreground cyan)
-   (doom-modeline-evil-visual-state :foreground yellow)
-   (doom-modeline-info :inherit 'mode-line-emphasis)
-   (doom-modeline-lsp-success :inherit 'mode-line-emphasis :weight 'bold)
-   (doom-modeline-persp-name :inherit 'mode-line-emphasis :weight 'bold)
-   (doom-modeline-project-dir :inherit 'mode-line-emphasis :weight 'bold)
-   (doom-modeline-project-parent-dir :inherit 'mode-line-emphasis :weight 'bold)
-   (doom-modeline-urgent :inherit 'mode-line-emphasis)
-   (doom-modeline-warning :inherit 'mode-line-emphasis)
+   (doom-modeline-bar :background (if doom-two-brighter-modeline modeline-bg highlight))
+   (doom-modeline-buffer-file :inherit 'mode-line-buffer-id :weight 'bold)
+   (doom-modeline-buffer-path :inherit 'mode-line-emphasis :weight 'bold)
+   (doom-modeline-buffer-project-root :foreground green :weight 'bold)
+   ;;;; elscreen
+   (elscreen-tab-other-screen-face :background "#353a42" :foreground "#1e2022")
    ;;;; ivy
-   (counsel-active-mode :foreground (doom-lighten base6 0.1))
-   (ivy-minibuffer-match-face-2 :foreground (doom-lighten base6 0.1) :weight 'extra-bold)
-   ;;;; js2-mode
-   (js2-jsdoc-tag              :foreground magenta)
-   (js2-object-property        :foreground cyan)
-   (js2-object-property-access :foreground cyan)
-   (js2-function-param         :foreground violet)
-   (js2-jsdoc-type             :foreground base8)
-   (js2-jsdoc-value            :foreground cyan)
-   ;;;; lsp-mode
-   (lsp-lens-face              :foreground base7 :height 0.8)
-   ;;;; man <built-in>
-   (Man-overstrike :inherit 'bold :foreground magenta)
-   (Man-underline :inherit 'underline :foreground blue)
-   ;;;; org <built-in>
-   ((org-block &override) :background base2)
-   ((org-block-background &override) :background base2)
-   ((org-block-begin-line &override) :background base2)
-   ;;;; org-pomodoro
-   (org-pomodoro-mode-line :inherit 'mode-line-emphasis) ; unreadable otherwise
-   (org-pomodoro-mode-line-overtime :inherit 'org-pomodoro-mode-line)
-   (org-pomodoro-mode-line-break :inherit 'org-pomodoro-mode-line)
-   ;;;; rainbow-delimiters
-   (rainbow-delimiters-depth-1-face :foreground magenta)
-   (rainbow-delimiters-depth-2-face :foreground orange)
-   (rainbow-delimiters-depth-3-face :foreground green)
-   (rainbow-delimiters-depth-4-face :foreground cyan)
-   (rainbow-delimiters-depth-5-face :foreground violet)
-   (rainbow-delimiters-depth-6-face :foreground yellow)
-   (rainbow-delimiters-depth-7-face :foreground blue)
-   (rainbow-delimiters-depth-8-face :foreground teal)
-   (rainbow-delimiters-depth-9-face :foreground dark-cyan)
+   (ivy-current-match :background dark-blue :distant-foreground base0 :weight 'normal)
+   ;;;; LaTeX-mode
+   (font-latex-math-face :foreground green)
+   ;;;; markdown-mode
+   (markdown-markup-face :foreground base5)
+   (markdown-header-face :inherit 'bold :foreground red)
+   ((markdown-code-face &override) :background (doom-lighten base3 0.05))
    ;;;; rjsx-mode
-   (rjsx-tag :foreground blue)
-   (rjsx-attr :foreground cyan :slant 'italic :weight 'medium)
-   ;;;; tooltip
-   (tooltip :background base2 :foreground fg)
-   ;;;; treemacs
-   (treemacs-root-face :foreground fg :weight 'ultra-bold :height 1.2)
-   (doom-themes-treemacs-root-face :foreground fg :weight 'ultra-bold :height 1.2)
-   (doom-themes-treemacs-file-face :foreground fg)
-   (treemacs-directory-face :foreground fg)
-   (treemacs-git-modified-face :foreground blue)))
+   (rjsx-tag :foreground red)
+   (rjsx-attr :foreground orange)
+   ;;;; solaire-mode
+   (solaire-mode-line-face
+    :inherit 'mode-line
+    :background modeline-bg-alt
+    :box (if -modeline-pad `(:line-width ,-modeline-pad :color ,modeline-bg-alt)))
+   (solaire-mode-line-inactive-face
+    :inherit 'mode-line-inactive
+    :background modeline-bg-inactive-alt
+    :box (if -modeline-pad `(:line-width ,-modeline-pad :color ,modeline-bg-inactive-alt))))
+
+  ;;;; Base theme variable overrides-
+  ())
 
 ;;; doom-two-theme.el ends here
