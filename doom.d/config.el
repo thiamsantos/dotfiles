8d1028bb72f210298b3ecca85b1bf804bc08db67
Thiago Santos <thia.mdossantos@gmail.com>
2021-11-03 11:50:06 -0300

    Disable projectile search cache

--- doom.d/config.el
+++ doom.d/config.el
@@ -35,6 +35,8 @@
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
 
+;; Disable projectile caching
+(setq projectile-enable-caching nil)
 
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
